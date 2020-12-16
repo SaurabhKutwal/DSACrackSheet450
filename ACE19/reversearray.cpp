86947ae48566c0758c7df374f03817f24342e838
Saurabh Kutwal <kutwalsaurabh19@gmail.com>
2020-12-16 21:19:46 +0530

    recursive approach

--- ACE19/reversearray.cpp
+++ ACE19/reversearray.cpp
@@ -1,5 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+//Recursive Approach
+void reverseArray(int arr[], int start, int end){
+  if(start<=end){
+    swap(arr[start],arr[end]);
+    reverseArray(arr,start+1,end-1);
+  }
+}
+
 int main(){
   int n;
   cin >> n;
@@ -8,11 +17,13 @@ int main(){
     cin >> arr[i];
   }
   int i=0,j=n-1;
-  while(i<=j){
-    swap(arr[i],arr[j]);
-    i++;
-    j--;
-  }
+  // while(i<=j){
+  //   swap(arr[i],arr[j]);
+  //   i++;
+  //   j--;
+  // }
+
+  reverseArray(arr,0,n-1);
 
   for(int i=0;i<n;i++){
     cout << arr[i] << " ";
