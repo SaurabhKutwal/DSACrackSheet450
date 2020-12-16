e81c1444db17047bb3a75f748e09673673548bb7
Saurabh Kutwal <kutwalsaurabh19@gmail.com>
2020-12-16 23:56:08 +0530

    if want more times

--- ACE19/cylindricalRotateArrayBy1.cpp
+++ ACE19/cylindricalRotateArrayBy1.cpp
@@ -11,6 +11,10 @@ int main(){
   for(int i=n-1;i>0;i--){
     swap(arr[i],arr[i-1]);
   }
+  //if 2 times
+  // for(int i=n-1;i>1;i--){
+  //   swap(arr[i],arr[i-1]);
+  // }
 
   for(int i=0;i<n;i++){
     cout << arr[i] << " ";
